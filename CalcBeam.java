521e2f79aa93c80fcb18dc8660ea43186b288c84
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-07-08 18:01:23 +0300

    HomeWork
    Класс подбора необходимых геометрических характеристик сечения стальной балки

---
+++ CalcBeam.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class CalcBeam {
+    private final int DESIGN_RESISTANCE = 240000000; //Расчетное сопротивление в Па
+    private final long ELASTIC_MODULE = 206000000000L; //Модуль упругости в Па
+    private  final float LIMIT_OF_DEFORMATION = 0.005f;//Предельные значения  прогиба балки по отношению к длине
+    private  final float LOAD_SAFETY_FACTOR = 1.35f;// Коэффициент надежности по нагрузке
+
+    private float lengthBeam; //Длина балки в метрах
+    private float loadBeam; //Нагрузка на балку в кПа
+
+    public CalcBeam(float lengthBeam, float loadBeam) {
+        this.lengthBeam = lengthBeam;
+        this.loadBeam = loadBeam;
+        double maxMoment = calcBendingMoment(lengthBeam,loadBeam);
+        double minMomentOfResistance = calcMomentOfResistance(maxMoment);
+        double minMomentOfInertia = calcMomentOfInertia(lengthBeam,loadBeam);
+        System.out.printf("Максимальный изгибающий момент: %.2f кН*м \n",maxMoment);
+        System.out.printf("Требуемый минимальный момент сопротивления: %.1f см3 \n",minMomentOfResistance);
+        System.out.printf("Требуемый минимальный момент инерции: %.1f см4 \n",minMomentOfInertia);
+
+    }
+
+    public float getLengthBeam() {
+        return lengthBeam;
+    }
+
+    public void setLengthBeam(float lengthBeam) {
+        if (lengthBeam > 0) {
+            this.lengthBeam = lengthBeam;
+        } else {
+            System.out.println("Проверьте длину балки");
+        }
+    }
+
+    public float getLoadBeam() {
+        return loadBeam;
+    }
+
+    public void setLoadBeam(float loadBeam) {
+        if (loadBeam != 0) {
+            this.loadBeam = loadBeam;
+        } else {
+            System.out.println("Нагрузка не должна быть равна 0");
+        }
+    }
+
+    private double calcBendingMoment(float lengthBeam, float loadbeam) { //Расчет максимального изгибающего момента балки в кН/м
+        this.lengthBeam = lengthBeam;
+        this.loadBeam = loadbeam;
+        double maxMoment = loadbeam * Math.pow(lengthBeam, 2) / 8;
+        return maxMoment;
+    }
+
+    private double calcMomentOfResistance(double maxMoment) { //Расчет минимального требуемого момента сопротивления в см3
+        double minMomentOfResistance = (maxMoment * 1000 / DESIGN_RESISTANCE) * 1000000;
+        return minMomentOfResistance;
+    }
+
+    private double calcMomentOfInertia(float lengthBeam, float loadBeam) {//Расчет минимального момента инерции в см4
+        double minMomentOfInertia = 5*loadBeam*1000*Math.pow(lengthBeam,3)/(384*ELASTIC_MODULE*LIMIT_OF_DEFORMATION*LOAD_SAFETY_FACTOR)*100000000;
+    return minMomentOfInertia;
+    }
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CalcBeam beam1 = new CalcBeam( 5.7f,20.5f);
+
+
+    }
+
+}
